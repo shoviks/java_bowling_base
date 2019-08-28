863c1bf3d30cebd7b7eed7d84607c6a002d76648
Alex McConnell <alexmc3@gmail.com>
2019-08-28 12:56:33 -0400

    Initial commit.

---
+++ src/test/java/com/bowling/BowlingTest.java
@@ -0,0 +1,18 @@
+package com.bowling;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BowlingTest {
+
+    private Bowling bowling;
+
+    @Test
+    public void shouldExist() {
+
+        assertNotNull(new Bowling());
+    }
+
+}
